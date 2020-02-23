a8dc9550b4d983699aaf1715739b1947066d3545
ankonovalov1 <ankonovalov@outlook.com>
2020-02-23 14:27:40 +0300

    Начало работы и немного гит игнора

---
+++ DevEdu/DevEdu.Data/Models/ThemeDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEdu.Data.Models
+{
+    public class ThemeDetails
+    { 
+        public int? Id { get; set; }
+        public int ProgramDetailId { get; }
+        public string Topic { get; set; }
+    }
+}
---
+++ DevEdu/DevEdu.Data/Storages/CourseStorages.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEdu.Data.Storages
+{
+    public class CourseStorages
+    {
+    }
+}
