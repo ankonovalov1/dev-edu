39495f91f30a93d1c668c90f6d592f15de9791b2
ankonovalov1 <ankonovalov@outlook.com>
2020-02-23 16:04:06 +0300

    ЧУТКА КОДА

---
+++ DevEdu/DevEdu.Data/Storages/CourseStorage.cs
@@ -0,0 +1,46 @@
+﻿using DevEdu.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace DevEdu.Data.Storages
+{
+    public class CourseStorage
+    {
+        private const string connectionString = @"Data Source=185.26.112.224;Initial Catalog=DevEducation;User ID=dev;Password=********;";
+
+        public List<ThemeDetails> GetThemeDeatails()
+        {
+            List<ThemeDetails> res = new List<ThemeDetails>();
+            string sqlExpression = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                ThemeDetails themeDetail = new ThemeDetails() { Id = reader.GetInt32(0), Topic = reader.GetString(2) };
+                                res.Add(themeDetail);
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            }
+            return res;
+        }
+    }
+}
--- DevEdu/DevEdu/Controllers/CourseController.cs
+++ DevEdu/DevEdu/Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevEdu.Data.Storages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,11 @@ namespace DevEdu.Controllers
     [ApiController]
     public class CourseController : ControllerBase
     {
+        CourseStorage storage;
+
+        public CourseController()
+        {
+            storage = new CourseStorage();
+        }
     }
 }
